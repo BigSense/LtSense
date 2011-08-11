9e6313017deb67d9af8afef4b5b2c37ba7e9fbf1
Sumit Khanna <sumit@penguindreams.org>
2011-08-11 17:00:19 -0400

    Needs testing, but signature code written

--- greenstation/signature.py
+++ greenstation/signature.py
@@ -2,28 +2,63 @@
 
 from Crypto.PublicKey import RSA
 from Crypto.Hash import SHA
+from os import path
+import logging
 
-class Security(object):
+class AbstractSecurity(object):
   
   def __init__(self):
     object.__init__(self)
     self.dataDir = None
     
-  def 
+  def initalize_security(self):
+    pass
+
+  def sign_data(self,data):
+    return 'Unimplemented'
+
+  def encrypt_data(self,data):
+    return 'Unimplemented'
+
+class SignatureSecurity(AbstractSecurity):
+
+  def __init__(self):
+    AbstratSecurity.__init__(self)
+    self.ready = False
+    self.privateKeyFile = 'key'
+    self.publicKeyFile = 'key.pub'
+    self.keySize = 2048
+
+  def initalize_security(self):
+    if self.dataDir != None:
+      if not path.isdir(self.dataDir):
+        logging.error('Security Data Directory does not exist or is not a directory: %s' % self.dataDir)
+      else:
+
+        priPath = path.join(self.dataDir,self.privateKeyFile)
+	pubPath = path.join(self.dataDir,self.publicKeyFile)
+
+        if path.isfile(priPath) and path.isfile(pubPath): 
+	  self.__private_key = RSA.importKey(priPath)
+	  with open(pubPath,'r') as fpub:
+	    self.__public_key = fpub.read()
+	  self.ready = True
+	else:
+          # private_key is a string suitable for storing on disk for retrieval later
+          # public_key is a string suitable for sending to the server
+          # The server should store this along with the client ID for verification
+          key = RSA.generate(self.keySize)
+          self.__private_key = key.exportKey()
+          self.__public_key = key.publickey().exportKey()
+          with open(pubPath,'w') as fpub:
+	    fpub.write(self.__public_key)
+	  with open(priPath,'w') as fpri:
+	    fpri.write(self.__private_key)
+          self.ready = True
 
 
-from Crypto.PublicKey import RSA
-key = RSA.generate(2048)
-private_key = key.exportKey()
-public_key = key.publickey().exportKey()
-# private_key is a string suitable for storing on disk for retrieval later
-# public_key is a string suitable for sending to the server
-# The server should store this along with the client ID for verification
-
-
-
-key = RSA.importKey(private_key)
-# where private_key is read from wherever you stored it previously
-digest = SHA.new(message).digest()
-signature = key.sign(digest, None)
-# attach signature to the message however you wish
+  def sign_data(self,data):
+    if self.ready = True:
+      digest = SHA.new(data).digest()
+      return self.__private_key.sign(digest, None)
+    return ''
