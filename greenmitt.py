3092e18251d9f7e9ea89b8f639e5442e05fd78c5
Sumit Khanna <sum.notify@gmail.com>
2011-09-01 15:42:19 -0400

    Moved things to more appropiate directories

--- greenmitt.py
+++ greenmitt.py
@@ -4,7 +4,8 @@ from optparse import OptionParser,OptionGroup
 import sys
 import logging 
 import greenstation.loader
-import datetime 
+import datetime
+from os.path import basename
 
 def logfile_arg():
    def func(option,opt_str,value,parser):
@@ -13,7 +14,7 @@ def logfile_arg():
          parser.rargs.pop(0)
       else:
          #defaults to program_name_YYYY-MM-DD_HHMMSS.log
-         val = sys.argv[0] + '_' + datetime.datetime.now().strftime('%Y-%m-%d_%H%M%S') + '.log'
+         val = 'var/log/' + basename(sys.argv[0]) + '_' + datetime.datetime.now().strftime('%Y-%m-%d_%H%M%S') + '.log'
       setattr(parser.values,option.dest,val)
    return func
 
@@ -22,8 +23,8 @@ if __name__ == '__main__':
    parser = OptionParser(usage="%prog [-dvq] [-l <logfile>] [-c config ]",
                          description="Greenstation sensor relay service.\n", version="%prog 0.1 alpha", epilog='Copyright 2011 Sumit Khanna. PenguinDreams.org')
    parser.add_option('-d','--debug',action='store_true',help='show additional debugging output')
-   parser.add_option('-l','--logfile',action='callback',callback=logfile_arg(),help='store output to logfile [default: %s_yyyy-mm-dd-hhmmss.log]' % sys.argv[0],metavar='FILE',dest='logfile')
-   parser.add_option('-c','--config',action='store',dest='config',help='configuration file [default: %default]',default='gm.config')
+   parser.add_option('-l','--logfile',action='callback',callback=logfile_arg(),help='store output to logfile [default: var/log/%s_yyyy-mm-dd-hhmmss.log]' % basename(sys.argv[0]),metavar='FILE',dest='logfile')
+   parser.add_option('-c','--config',action='store',dest='config',help='configuration file [default: %default]',default='etc/gm.config')
    parser.set_defaults(verbose=True)
    parser.add_option('-v', action='store_true', dest='verbose', help='verbose output (default, combine with -d for additional information)')
    parser.add_option('-q', action='store_false', dest='verbose', help='run silent')
