f88b036bc67ce4fa64e8e13d0908da53d47e2027
Sumit Khanna <sumit@penguindreams.org>
2014-08-28 23:51:28 +1200

    FIXED! Fully working dependency bootstrapping!

--- ltsense/config.py
+++ ltsense/config.py
@@ -17,7 +17,7 @@ sample_rate = float
     type = option('sense.xml')
     transports = string
     sensors = list
-      [Identification]
+      [[[Identification]]]
         type = option("name", "mac", "uuid")
         adapter = string
         name = string
@@ -32,11 +32,16 @@ sample_rate = float
       [[[Queue]]]
         type = option('memory','sqlite')
         data = string
-      [[Security]]
+      [[[Security]]]
         type = option('none','rsa','m2')
         data_dir = string
         key_file = string
         key_size = integer
+
+[Handlers]
+  [[__many__]]
+    type = option('virtual/temp')
+    sensors = string
 
 [Sensors]
   [[__many__]]
@@ -59,7 +64,7 @@ sample_rate = float
     return getattr(mod,clss)()
 
   def _set_args(self,obj,key,value):
-    logging.debug('Setting attribute %s to %s for class %s (type: %s)' % (key,value,obj,type(value)))
+    logging.debug('Setting attribute {0} to {1} for class {2} (type: {3})'.format(key,value,obj,type(value)))
     setattr(obj,key,value)
 
 
@@ -68,56 +73,69 @@ sample_rate = float
       self._delayed_eval[var] = {}
     self._delayed_eval[var][attr] = val
 
-  def __create_variable(self,cfg,section,c,var):
-    tp = cfg[c]['type']
-    logging.debug('Creating variable {0} with type {1}'.format(c,tp))
-    logging.debug('ltsense.{0}.{1}'.format(section.lower(),self.types[section][cfg[c]['type']]))
-    self._object_map[var] = self._load_obj('{0}.{1}'.format(section.lower(),self.types[section][cfg[c]['type']]))
+  def __create_variable(self,cfg,section,var):
+    tp = cfg['type']
+    logging.debug('Creating variable {0} with type {1}'.format(var,tp))
+    if tp == 'none':
+      self._object_map[var] = None
+    else:
+      logging.debug('\t>>ltsense.{0}.{1}'.format(section.lower(),self.types[section][tp]))
+      self._object_map[var] = self._load_obj('{0}.{1}'.format(section.lower(),self.types[section][tp]))
 
   def __process(self,cfg,section=None,variable=None):
     """Recursively processes a configuration object"""
-    logging.debug(">>Entry :: Section: {0} :: Var {1}".format(section,variable))
-    logging.debug("\t<<" + str(cfg))
     for c in cfg:
-      logging.debug("\t>>{0}".format(c))
       if type(cfg[c]) is Section:
         if c == 'General':
           logging.debug('Loading General Section')
+          logging.debug('Sample Rate: {0}'.format(cfg[c]['sample_rate']))
           self._controller.sample_rate = cfg[c]['sample_rate']
         elif c[0].isupper() and section is None:
           logging.debug('Loading Section: {0} '.format(c))
           self.__process(cfg[c],c)
         elif c[0].isupper() and section is not None:
-          logging.debug('Loading Anonymous Var Sub Section {0}'.format(c))
+          av = "$${0}.{1}".format(section.lower(),c)
+          logging.debug('Loading Anonymous Variable {0} with Type: {1}'.format(av,c))
+          self.__create_variable(cfg[c],c,av)
+          self.__add_delay_eval(variable,section.lower(),av)
+          self.__process(cfg[c],c,av)
         else:
-          logging.debug('Loading Variable: {0}'.format(c))
-          self.__process(cfg[c],section,c)
+          var = "${0}".format(c)
+          logging.debug('Loading Variable: {0}'.format(var))
+          self.__create_variable(cfg[c],section,var)
+          self.__process(cfg[c],section,var)
       else:
         #Anything starting with $ or is a list has delayed evaluation
         if (isinstance(cfg[c],basestring) and cfg[c][0] == '$') or isinstance(cfg[c],list):
           logging.debug("Delay evaluation for {0}".format(cfg[c]))
-          #self.__add_delay_eval(variable,c,cfg[c])
+          self.__add_delay_eval(variable,c,cfg[c])
         elif c == 'type':
           pass
         else:
-          logging.debug('Setting attribute {0} for {1}'.format(c,variable))
-          #self._set_args(self._object_map[variable],c,cfg[c])
+          self._set_args(self._object_map[variable],c,cfg[c])
 
 
   def __eval_item(self,item):
-    """Used for deferred evaluation variables (those that start with $) and lists.
+    """Used vy __eval_delays for deferred evaluation variables (those that start with $) and lists.
       Items in list starting with $ will be substituted for defined objects.
-      All other list items will be treated as string.
+      Non-list items don.
     """
     if isinstance(item,list):
       #Substitute all variables (start with $)
       for i,j in enumerate(item):
         if j[0] == '$':
-          item[i] = self._object_map(j[1:])
+          item[i] = self._object_map[j[1:]]
       return item
     else:
-      return item[1:]
+      return self._object_map[item]
 
+  def __eval_delays(self):
+    """Call after a successful __process to assign all delayed variables"""
+    for var in self._delayed_eval:
+      for i,j in self._delayed_eval[var].items():
+        item = self.__eval_item(j)
+        logging.debug("Setting object {0}, {1} = {2}".format(var,i,item))
+        self._set_args(self._object_map[var], i, item )
 
   def __init__(self, filename):
 
@@ -160,19 +178,11 @@ sample_rate = float
       print("Invalid")
       exit(3)
 
-    for i in cfg['Transport']:
-      print(cfg['Transport'])
-      print(i)
-
-    """self.__process(cfg)
-    for var in self._delayed_eval:
-      for i in self._delayed_eval[var]:
-        item = self.__eval_item(i[1])
-        logging.debug("Setting object {0}, {1} = {2}".format(var,i[0],item))
-        self._set_args(self._object_map[var], i[0], item )
+    self.__process(cfg)
+    self.__eval_delays()
 
     self._controller.data_handlers = self._data_handlers
-    self._controller.sensor_handlers = self._sensor_handlers"""
+    self._controller.sensor_handlers = self._sensor_handlers
 
 
 
