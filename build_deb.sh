867d2bd6bb2063293a5e40481c7c55e5b856d7ee
Sumit Khanna <sumit@catalyst.net.nz>
2014-12-23 10:02:35 +1300

    Added force flag to fpm

--- build_deb.sh
+++ build_deb.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-fpm -t deb -s python --python-install-lib /usr/lib/python2.7/dist-packages --python-bin=$(which python2.7) --no-python-fix-name setup.py
+fpm -f -t deb -s python --python-install-lib /usr/lib/python2.7/dist-packages --python-bin=$(which python2.7) --no-python-fix-name setup.py
